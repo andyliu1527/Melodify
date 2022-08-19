a33773ec458d28ebb52009a450aca43b4e561cc0
Andy Liu <andyliu1527@gmail.com>
2022-08-19 11:45:56 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all;
+
+user_1 = User.create(username: "Andy", email:"cloudy@gmail.com", password: "123456");
+user_2 = User.create(username: "Andrea", email: "padgzmn@gmail.com", password: "123456");
+demoUser = User.create(username: "demoUser", email: "demoUser@gmail.com", password: "demo123");
